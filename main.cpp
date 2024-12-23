8140b4342739de274927fe1735b55d9f94c13ba6
Ethan Klasky <klaskyethan@gmail.com>
2024-12-23 12:03:22 -0500

    making progress

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <hdf5.h>
 #include <cstdlib>
 #include <iostream>
+#include <adios2.h>
 
 #define H5FILE_NAME "SDS_row.h5"
 #define DATASETNAME "IntArray"
@@ -9,83 +10,90 @@
 #define NY          5
 #define RANK        2
 
-int main(int argc, char **argv)
+// ASSUMING THIS CODE ONLY RUNS WITH MPI 
+
+int main(int argc , char** argv)
 {
     /*
      * HDF5 APIs definitions
      */
-    hid_t   file_id, dset_id;    /* file and dataset identifiers */
-    hid_t   filespace, memspace; /* file and memory dataspace identifiers */
+    hid_t   file_id , dset_id;    /* file and dataset identifiers */
+    hid_t   filespace , memspace; /* file and memory dataspace identifiers */
     hsize_t dimsf[2];            /* dataset dimensions */
-    int    *data;                /* pointer to data buffer to write */
+    int* data;                /* pointer to data buffer to write */
     hsize_t count[2];            /* hyperslab selection parameters */
     hsize_t offset[2];
     hid_t   plist_id; /* property list identifier */
     int     i;
     herr_t  status;
 
+
     /*
      * MPI variables
      */
-    int      mpi_size, mpi_rank;
+    int      mpi_size , mpi_rank;
     MPI_Comm comm = MPI_COMM_WORLD;
     MPI_Info info = MPI_INFO_NULL;
-
+    int provided;
+    MPI_Init_thread(&argc , &argv , MPI_THREAD_MULTIPLE , &provided);
+    if (provided < MPI_THREAD_MULTIPLE) {
+        std::cerr << "Warning: Insufficient MPI threading support" << std::endl;
+    }
     /*
      * Initialize MPI
      */
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(comm, &mpi_size);
-    MPI_Comm_rank(comm, &mpi_rank);
+    MPI_Comm_size(comm , &mpi_size);
+    MPI_Comm_rank(comm , &mpi_rank);
 
     std::cout << "Number of processes: " << mpi_size << std::endl;
-
+    adios2::ADIOS adios;
+    adios2::IO io = adios.DeclareIO("WriteIO");
     /*
      * Set up file access property list with parallel I/O access
      */
     plist_id = H5Pcreate(H5P_FILE_ACCESS);
-    H5Pset_fapl_mpio(plist_id, comm, info);
+    H5Pset_fapl_mpio(plist_id , comm , info);
 
     /*
      * Create a new file collectively and release property list identifier.
      */
-    file_id = H5Fcreate(H5FILE_NAME, H5F_ACC_TRUNC, H5P_DEFAULT, plist_id);
+    file_id = H5Fcreate(H5FILE_NAME , H5F_ACC_TRUNC , H5P_DEFAULT , plist_id);
     H5Pclose(plist_id);
 
     /*
      * Create the dataspace for the dataset.
      */
-    dimsf[0]  = NX;
-    dimsf[1]  = NY;
-    filespace = H5Screate_simple(RANK, dimsf, NULL);
+    dimsf[0] = NX;
+    dimsf[1] = NY;
+    filespace = H5Screate_simple(RANK , dimsf , NULL);
 
     /*
      * Create the dataset with default properties and close filespace.
      */
     dset_id =
-        H5Dcreate(file_id, DATASETNAME, H5T_NATIVE_INT, filespace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        H5Dcreate(file_id , DATASETNAME , H5T_NATIVE_INT , filespace , H5P_DEFAULT , H5P_DEFAULT , H5P_DEFAULT);
     H5Sclose(filespace);
 
     /*
      * Each process defines dataset in memory and writes it to the hyperslab
      * in the file.
      */
-    count[0]  = dimsf[0] / mpi_size;
-    count[1]  = dimsf[1];
+    count[0] = dimsf[0] / mpi_size;
+    count[1] = dimsf[1];
     offset[0] = mpi_rank * count[0];
     offset[1] = 0;
-    memspace  = H5Screate_simple(RANK, count, NULL);
+    memspace = H5Screate_simple(RANK , count , NULL);
 
     /*
      * Select hyperslab in the file.
      */
     filespace = H5Dget_space(dset_id);
-    H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
+    H5Sselect_hyperslab(filespace , H5S_SELECT_SET , offset , NULL , count , NULL);
 
     /*
      * Initialize data buffer
      */
-    data = (int *)malloc(sizeof(int) * count[0] * count[1]);
+    data = (int*)malloc(sizeof(int) * count[0] * count[1]);
     for (i = 0; i < count[0] * count[1]; i++) {
         data[i] = mpi_rank + 10;
     }
@@ -94,9 +102,9 @@ int main(int argc, char **argv)
      * Create property list for collective dataset write.
      */
     plist_id = H5Pcreate(H5P_DATASET_XFER);
-    H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
+    H5Pset_dxpl_mpio(plist_id , H5FD_MPIO_COLLECTIVE);
 
-    status = H5Dwrite(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data);
+    status = H5Dwrite(dset_id , H5T_NATIVE_INT , memspace , filespace , plist_id , data);
     free(data);
 
     /*
