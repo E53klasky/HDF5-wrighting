b194bb655634234ec36e7c21e7a3d137b801749a
Ethan Klasky <klaskyethan@gmail.com>
2024-12-31 10:26:54 -0500

    cmake is now using mpi c++ compiler idk

--- build/CMakeFiles/phdf5_example.dir/main.cpp.o.d
+++ build/CMakeFiles/phdf5_example.dir/main.cpp.o.d
@@ -1,7 +1,10 @@
 CMakeFiles/phdf5_example.dir/main.cpp.o: \
  /home/adios/Programs/HDF5-test/HDF5-wrighting/main.cpp \
- /usr/include/stdc-predef.h /home/adios/local/include/mpi.h \
- /usr/include/c++/11/cstdint \
+ /usr/include/stdc-predef.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h \
+ /usr/include/c++/11/iostream \
  /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
  /usr/include/features.h /usr/include/features-time64.h \
@@ -12,101 +15,23 @@ CMakeFiles/phdf5_example.dir/main.cpp.o: \
  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/cpu_defines.h \
- /usr/include/c++/11/pstl/pstl_config.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
- /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
- /usr/include/x86_64-linux-gnu/bits/types.h \
- /usr/include/x86_64-linux-gnu/bits/typesizes.h \
- /usr/include/x86_64-linux-gnu/bits/time64.h \
- /usr/include/x86_64-linux-gnu/bits/wchar.h \
- /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
- /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
- /home/adios/local/include/mpio.h /home/adios/local/include/mpi.h \
- /home/adios/local/include/hdf5.h /home/adios/local/include/H5public.h \
- /home/adios/local/include/H5pubconf.h \
- /home/adios/local/include/H5version.h \
- /usr/include/x86_64-linux-gnu/sys/types.h \
- /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h /usr/include/endian.h \
- /usr/include/x86_64-linux-gnu/bits/endian.h \
- /usr/include/x86_64-linux-gnu/bits/endianness.h \
- /usr/include/x86_64-linux-gnu/bits/byteswap.h \
- /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
- /usr/include/x86_64-linux-gnu/sys/select.h \
- /usr/include/x86_64-linux-gnu/bits/select.h \
- /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
- /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
- /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
- /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
- /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
- /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
- /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/limits.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/syslimits.h \
- /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
- /usr/include/x86_64-linux-gnu/bits/local_lim.h \
- /usr/include/linux/limits.h \
- /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
- /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
- /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
- /usr/include/x86_64-linux-gnu/bits/uio_lim.h /usr/include/inttypes.h \
- /home/adios/local/include/H5api_adpt.h \
- /home/adios/local/include/H5Apublic.h \
- /home/adios/local/include/H5Ipublic.h \
- /home/adios/local/include/H5Opublic.h \
- /home/adios/local/include/H5Lpublic.h \
- /home/adios/local/include/H5Tpublic.h \
- /home/adios/local/include/H5ACpublic.h \
- /home/adios/local/include/H5Cpublic.h \
- /home/adios/local/include/H5Dpublic.h \
- /home/adios/local/include/H5Epublic.h /usr/include/stdio.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
- /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
- /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
- /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
- /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
- /usr/include/x86_64-linux-gnu/bits/floatn.h \
- /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
- /home/adios/local/include/H5Epubgen.h \
- /home/adios/local/include/H5Fpublic.h \
- /home/adios/local/include/H5FDpublic.h \
- /home/adios/local/include/H5Gpublic.h \
- /home/adios/local/include/H5MMpublic.h \
- /home/adios/local/include/H5Ppublic.h \
- /home/adios/local/include/H5Zpublic.h \
- /home/adios/local/include/H5PLpublic.h \
- /home/adios/local/include/H5Rpublic.h \
- /home/adios/local/include/H5Spublic.h \
- /home/adios/local/include/H5FDcore.h \
- /home/adios/local/include/H5FDfamily.h \
- /home/adios/local/include/H5FDlog.h /home/adios/local/include/H5FDmpi.h \
- /home/adios/local/include/H5FDmpio.h \
- /home/adios/local/include/H5FDmulti.h \
- /home/adios/local/include/H5FDsec2.h \
- /home/adios/local/include/H5FDstdio.h \
- /home/adios/local/include/H5FDdirect.h /usr/include/c++/11/cstdlib \
- /usr/include/stdlib.h /usr/include/x86_64-linux-gnu/bits/waitflags.h \
- /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
- /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
- /usr/include/alloca.h /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
- /usr/include/c++/11/bits/std_abs.h /usr/include/c++/11/iostream \
- /usr/include/c++/11/ostream /usr/include/c++/11/ios \
- /usr/include/c++/11/iosfwd /usr/include/c++/11/bits/stringfwd.h \
+ /usr/include/c++/11/pstl/pstl_config.h /usr/include/c++/11/ostream \
+ /usr/include/c++/11/ios /usr/include/c++/11/iosfwd \
+ /usr/include/c++/11/bits/stringfwd.h \
  /usr/include/c++/11/bits/memoryfwd.h /usr/include/c++/11/bits/postypes.h \
  /usr/include/c++/11/cwchar /usr/include/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
  /usr/include/x86_64-linux-gnu/bits/types/wint_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
  /usr/include/c++/11/exception /usr/include/c++/11/bits/exception.h \
  /usr/include/c++/11/bits/exception_ptr.h \
  /usr/include/c++/11/bits/exception_defines.h \
@@ -128,25 +53,46 @@ CMakeFiles/phdf5_example.dir/main.cpp.o: \
  /usr/include/c++/11/debug/assertions.h \
  /usr/include/c++/11/bits/stl_iterator.h \
  /usr/include/c++/11/bits/ptr_traits.h /usr/include/c++/11/debug/debug.h \
- /usr/include/c++/11/bits/predefined_ops.h \
+ /usr/include/c++/11/bits/predefined_ops.h /usr/include/c++/11/cstdint \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
  /usr/include/c++/11/bits/localefwd.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/c++locale.h \
  /usr/include/c++/11/clocale /usr/include/locale.h \
  /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/11/cctype \
- /usr/include/ctype.h /usr/include/c++/11/bits/ios_base.h \
- /usr/include/c++/11/ext/atomicity.h \
+ /usr/include/ctype.h /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/c++/11/bits/ios_base.h /usr/include/c++/11/ext/atomicity.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/gthr.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/gthr-default.h \
  /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
  /usr/include/x86_64-linux-gnu/bits/sched.h \
  /usr/include/x86_64-linux-gnu/bits/types/struct_sched_param.h \
  /usr/include/x86_64-linux-gnu/bits/cpu-set.h /usr/include/time.h \
  /usr/include/x86_64-linux-gnu/bits/time.h \
  /usr/include/x86_64-linux-gnu/bits/timex.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/struct_tm.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/struct_itimerspec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h \
  /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
  /usr/include/x86_64-linux-gnu/bits/types/struct___jmp_buf_tag.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/atomic_word.h \
  /usr/include/x86_64-linux-gnu/sys/single_threaded.h \
  /usr/include/c++/11/bits/locale_classes.h /usr/include/c++/11/string \
@@ -165,7 +111,22 @@ CMakeFiles/phdf5_example.dir/main.cpp.o: \
  /usr/include/c++/11/bits/stl_construct.h /usr/include/c++/11/string_view \
  /usr/include/c++/11/bits/functional_hash.h \
  /usr/include/c++/11/bits/string_view.tcc \
- /usr/include/c++/11/ext/string_conversions.h /usr/include/c++/11/cstdio \
+ /usr/include/c++/11/ext/string_conversions.h /usr/include/c++/11/cstdlib \
+ /usr/include/stdlib.h /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/alloca.h /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/c++/11/bits/std_abs.h /usr/include/c++/11/cstdio \
+ /usr/include/stdio.h /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
  /usr/include/c++/11/cerrno /usr/include/errno.h \
  /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
  /usr/include/x86_64-linux-gnu/asm/errno.h \
@@ -187,95 +148,4 @@ CMakeFiles/phdf5_example.dir/main.cpp.o: \
  /usr/include/c++/11/bits/locale_facets.tcc \
  /usr/include/c++/11/bits/basic_ios.tcc \
  /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
- /usr/include/c++/11/bits/istream.tcc /usr/local/include/adios2.h \
- /usr/local/include/adios2/common/ADIOSConfig.h \
- /usr/local/include/adios2/common/ADIOSMacros.h \
- /usr/local/include/adios2/common/ADIOSTypes.h \
- /usr/include/c++/11/cstddef /usr/include/c++/11/complex \
- /usr/include/c++/11/cmath /usr/include/math.h \
- /usr/include/x86_64-linux-gnu/bits/math-vector.h \
- /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
- /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
- /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
- /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
- /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
- /usr/include/c++/11/bits/specfun.h /usr/include/c++/11/limits \
- /usr/include/c++/11/tr1/gamma.tcc \
- /usr/include/c++/11/tr1/special_function_util.h \
- /usr/include/c++/11/tr1/bessel_function.tcc \
- /usr/include/c++/11/tr1/beta_function.tcc \
- /usr/include/c++/11/tr1/ell_integral.tcc \
- /usr/include/c++/11/tr1/exp_integral.tcc \
- /usr/include/c++/11/tr1/hypergeometric.tcc \
- /usr/include/c++/11/tr1/legendre_function.tcc \
- /usr/include/c++/11/tr1/modified_bessel_func.tcc \
- /usr/include/c++/11/tr1/poly_hermite.tcc \
- /usr/include/c++/11/tr1/poly_laguerre.tcc \
- /usr/include/c++/11/tr1/riemann_zeta.tcc /usr/include/c++/11/sstream \
- /usr/include/c++/11/bits/sstream.tcc /usr/include/c++/11/map \
- /usr/include/c++/11/bits/stl_tree.h \
- /usr/include/c++/11/ext/aligned_buffer.h \
- /usr/include/c++/11/bits/node_handle.h \
- /usr/include/c++/11/bits/stl_map.h /usr/include/c++/11/tuple \
- /usr/include/c++/11/utility /usr/include/c++/11/bits/stl_relops.h \
- /usr/include/c++/11/array /usr/include/c++/11/bits/uses_allocator.h \
- /usr/include/c++/11/bits/invoke.h \
- /usr/include/c++/11/bits/stl_multimap.h \
- /usr/include/c++/11/bits/erase_if.h /usr/include/c++/11/vector \
- /usr/include/c++/11/bits/stl_uninitialized.h \
- /usr/include/c++/11/bits/stl_vector.h \
- /usr/include/c++/11/bits/stl_bvector.h \
- /usr/include/c++/11/bits/vector.tcc \
- /usr/local/include/adios2/common/ADIOSConfig.h \
- /usr/local/include/adios2/common/ADIOSTypes.inl \
- /usr/local/include/adios2/common/ADIOSTypes.h \
- /usr/local/include/adios2/cxx11/ADIOS.h /usr/include/c++/11/functional \
- /usr/include/c++/11/bits/refwrap.h \
- /usr/include/c++/11/bits/std_function.h \
- /usr/include/c++/11/unordered_map /usr/include/c++/11/bits/hashtable.h \
- /usr/include/c++/11/bits/hashtable_policy.h \
- /usr/include/c++/11/bits/enable_special_members.h \
- /usr/include/c++/11/bits/unordered_map.h \
- /usr/include/c++/11/bits/stl_algo.h \
- /usr/include/c++/11/bits/algorithmfwd.h \
- /usr/include/c++/11/bits/stl_heap.h \
- /usr/include/c++/11/bits/stl_tempbuf.h \
- /usr/include/c++/11/bits/uniform_int_dist.h /usr/include/c++/11/memory \
- /usr/include/c++/11/bits/stl_raw_storage_iter.h \
- /usr/include/c++/11/bits/align.h /usr/include/c++/11/bit \
- /usr/include/c++/11/bits/unique_ptr.h \
- /usr/include/c++/11/bits/shared_ptr.h \
- /usr/include/c++/11/bits/shared_ptr_base.h \
- /usr/include/c++/11/bits/allocated_ptr.h \
- /usr/include/c++/11/ext/concurrence.h \
- /usr/include/c++/11/bits/shared_ptr_atomic.h \
- /usr/include/c++/11/bits/atomic_base.h \
- /usr/include/c++/11/bits/atomic_lockfree_defines.h \
- /usr/include/c++/11/backward/auto_ptr.h \
- /usr/include/c++/11/pstl/glue_memory_defs.h \
- /usr/include/c++/11/pstl/execution_defs.h \
- /usr/local/include/adios2/cxx11/IO.h \
- /usr/local/include/adios2/cxx11/Attribute.h \
- /usr/local/include/adios2/cxx11/Engine.h \
- /usr/local/include/adios2/cxx11/ADIOSView.h \
- /usr/local/include/adios2/cxx11/Types.h \
- /usr/local/include/adios2/cxx11/Variable.h \
- /usr/local/include/adios2/cxx11/Operator.h \
- /usr/local/include/adios2/common/ADIOSMacros.h \
- /usr/local/include/adios2/cxx11/VariableNT.h \
- /usr/local/include/adios2/cxx11/Group.h /usr/include/c++/11/set \
- /usr/include/c++/11/bits/stl_set.h \
- /usr/include/c++/11/bits/stl_multiset.h \
- /usr/local/include/adios2/cxx11/ADIOS.inl \
- /usr/local/include/adios2/cxx11/Attribute.h \
- /usr/local/include/adios2/cxx11/Engine.h \
- /usr/local/include/adios2/cxx11/Group.h \
- /usr/local/include/adios2/cxx11/IO.h \
- /usr/local/include/adios2/cxx11/Operator.h \
- /usr/local/include/adios2/cxx11/Query.h \
- /usr/local/include/adios2/cxx11/Types.h \
- /usr/local/include/adios2/cxx11/Variable.h \
- /usr/local/include/adios2/cxx11/fstream/ADIOS2fstream.h
+ /usr/include/c++/11/bits/istream.tcc
--- main.cpp
+++ main.cpp
@@ -1,140 +1,41 @@
 #include <mpi.h>
-#include <hdf5.h>
-#include <cstdlib>
 #include <iostream>
-#include <adios2.h>
 
-#define H5FILE_NAME "SDS_row.h5"
-#define DATASETNAME "IntArray"
-#define NX          8 /* dataset dimensions */
-#define NY          5
-#define RANK        2
-
-// ASSUMING THIS CODE ONLY RUNS WITH MPI 
-
-int main(int argc , char** argv)
-{
-    /*
-     * HDF5 APIs definitions
-     */
-    hid_t   file_id , dset_id;    /* file and dataset identifiers */
-    hid_t   filespace , memspace; /* file and memory dataspace identifiers */
-    hsize_t dimsf[2];            /* dataset dimensions */
-    int* data;                /* pointer to data buffer to write */
-    hsize_t count[2];            /* hyperslab selection parameters */
-    hsize_t offset[2];
-    hid_t   plist_id; /* property list identifier */
-    int     i;
-    herr_t  status;
-
-
-    /*
-     * MPI variables
-     */
+int main(int argc , char** argv) {
     int provided;
-    MPI_Init_thread(&argc , &argv , MPI_THREAD_MULTIPLE , &provided);
+    int init_result = MPI_Init_thread(&argc , &argv , MPI_THREAD_MULTIPLE , &provided);
+    if (init_result != MPI_SUCCESS) {
+        std::cerr << "MPI Initialization failed with error code " << init_result << std::endl;
+        MPI_Abort(MPI_COMM_WORLD , init_result);
+        return init_result;
+    }
 
-    // Move these after initialization
-    int mpi_size , mpi_rank;
+    std::cout << "MPI_Init_thread provided: " << provided << std::endl;
+
     MPI_Comm comm = MPI_COMM_WORLD;
     MPI_Info info = MPI_INFO_NULL;
 
-    MPI_Comm_size(comm , &mpi_size);
-    MPI_Comm_rank(comm , &mpi_rank);
-    if (provided < MPI_THREAD_MULTIPLE) {
-        std::cerr << "Warning: Insufficient MPI threading support" << std::endl;
+    int mpi_size;
+    int mpi_rank;
+
+    int comm_size_result = MPI_Comm_size(comm , &mpi_size);
+    if (comm_size_result != MPI_SUCCESS) {
+        std::cerr << "MPI_Comm_size failed with error code " << comm_size_result << std::endl;
+        MPI_Abort(comm , comm_size_result);
+        return comm_size_result;
     }
-    /*
-     * Initialize MPI
-     */
+    std::cout << "MPI_Comm_size: " << mpi_size << std::endl;
 
-    // #if ADIOS2_USE_MPI
-    // std::cout << "Number of processes: " << mpi_size << std::endl;
-    // // maybe it works now???
-    // adios2::ADIOS adios("adios2.xml" , comm);
-    // #else
-    // adios2::ADIOS adios;
-    // #endif
-    // adios2::IO bpIO = adios.DeclareIO("WriteIO");
-    // adios2::Engine bpWriter = bpIO.Open("output" , adios2::Mode::Write);
-    // adios2::Operator op = adios.DefineOperator("mgard" , "mgard");
-
-
-    /*
-     * Set up file access property list with parallel I/O access
-     */
-    plist_id = H5Pcreate(H5P_FILE_ACCESS);
-    H5Pset_fapl_mpio(plist_id , comm , info);
-
-    /*
-     * Create a new file collectively and release property list identifier.
-     */
-    file_id = H5Fcreate(H5FILE_NAME , H5F_ACC_TRUNC , H5P_DEFAULT , plist_id);
-    H5Pclose(plist_id);
-
-        /*
-         * Create the dataspace for the dataset.
-         */
-    dimsf[0] = NX;
-    dimsf[1] = NY;
-    filespace = H5Screate_simple(RANK , dimsf , NULL);
-
-    /*
-     * Create the dataset with default properties and close filespace.
-     */
-    dset_id =
-        H5Dcreate(file_id , DATASETNAME , H5T_NATIVE_INT , filespace , H5P_DEFAULT , H5P_DEFAULT , H5P_DEFAULT);
-    H5Sclose(filespace);
-
-    /*
-     * Each process defines dataset in memory and writes it to the hyperslab
-     * in the file.
-     */
-    count[0] = dimsf[0] / mpi_size;
-    count[1] = dimsf[1];
-    offset[0] = mpi_rank * count[0];
-    offset[1] = 0;
-    memspace = H5Screate_simple(RANK , count , NULL);
-
-    /*
-     * Select hyperslab in the file.
-     */
-    filespace = H5Dget_space(dset_id);
-    H5Sselect_hyperslab(filespace , H5S_SELECT_SET , offset , NULL , count , NULL);
-
-    /*
-     * Initialize data buffer
-     */
-    data = (int*)malloc(sizeof(int) * count[0] * count[1]);
-    for (i = 0; i < count[0] * count[1]; i++) {
-        data[i] = mpi_rank + 10;
+    int comm_rank_result = MPI_Comm_rank(comm , &mpi_rank);
+    if (comm_rank_result != MPI_SUCCESS) {
+        std::cerr << "MPI_Comm_rank failed with error code " << comm_rank_result << std::endl;
+        MPI_Abort(comm , comm_rank_result);
+        return comm_rank_result;
     }
+    std::cout << "MPI_Comm_rank: " << mpi_rank << std::endl;
 
-    /*
-     * Create property list for collective dataset write.
-     */
-    plist_id = H5Pcreate(H5P_DATASET_XFER);
-    H5Pset_dxpl_mpio(plist_id , H5FD_MPIO_COLLECTIVE);
-
-    status = H5Dwrite(dset_id , H5T_NATIVE_INT , memspace , filespace , plist_id , data);
-    free(data);
-
-    /*
-     * Close/release resources.
-     */
-    H5Dclose(dset_id);
-    H5Sclose(filespace);
-    H5Sclose(memspace);
-    H5Pclose(plist_id);
-    H5Fclose(file_id);
-
-    //bpWriter.Close();
-
-    if (mpi_rank == 0)
-        std::cout << "PHDF5 example finished with no errors" << std::endl;
-
+    std::cout << "Hello world from rank " << mpi_rank << " out of " << mpi_size << " processors" << std::endl;
 
     MPI_Finalize();
-
     return 0;
 }
